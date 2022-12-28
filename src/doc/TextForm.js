df169c68b9439d4a6db4f40edb2d5256a50b7dc2
Harshal Bolliwar <80410841+harshal0112@users.noreply.github.com>
2022-12-29 04:26:18 +0530

    (Undo/Redo) Some bugs fixed

--- src/doc/TextForm.js
+++ src/doc/TextForm.js
@@ -16,34 +16,49 @@ export default function TextForm(props) {
 
     const handleUndo = () => {
         if (undoStack.current.length > 1) {
-          redoStack.current.push(undoStack.current.pop());
-          setText(undoStack.current[undoStack.current.length - 1]);
+          let undo = undoStack.current.pop()
+          if (undo.at(-1) === "Ü"){
+            undo = undo.slice(0, -1);
+            setText(undo)
+          }
+          else
+          {
+            redoStack.current.push(undo)
+            setText(undoStack.current[undoStack.current.length - 1]);
+          }
         }
-      };
+        else if (undoStack.current.length === 1) {
+          setText("");
+          redoStack.current.push(undoStack.current.pop());
+        }
+      }
     
       const handleRedo = () => {
         if (redoStack.current.length > 0) {
-          undoStack.current.push(redoStack.current.pop());
+          const redo = redoStack.current.pop();
+          undoStack.current.push(redo);
+          console.log(redo)
           setText(undoStack.current[undoStack.current.length - 1]);
         }
       };
 
     const handleUpClick = ()=>{
         let newText = text.toUpperCase();
-        undoStack.current.push(text+" ");
+        undoStack.current.push(text+"Ü");
         setText(newText);
         props.showAlert('Text Converted to UpperCase.', 'success');
     }
 
     const handleLoClick = ()=>{
         let newText = text.toLowerCase();
+        undoStack.current.push(text+"Ü");
         setText(newText);
         props.showAlert('Text Converted to LowerCase.', 'success');
     }
 
     const handleClearClick = ()=>{
         let newText = "";
-        undoStack.current.push(text+" ");
+        undoStack.current.push(text+"Ü");
         redoStack.current.length = 0;
         setText(newText);
         props.showAlert('Text Cleared.', 'success');
@@ -72,8 +87,8 @@ export default function TextForm(props) {
         <button disabled={text.length === 0} className={`btn btn-${props.theme} mx-1 my-1`} onClick={handleClearClick}>Clear Text</button>
         <button disabled={text.length === 0} className={`btn btn-${props.theme} mx-1 my-1`} onClick={handleCopy}>Copy Text</button>
         <button disabled={text.match(/^.*\s{2,}.*$/) === null} className={`btn btn-${props.theme} mx-1 my-1`} onClick={handleExtraSpaces}>Remove Extra Spaces</button>
-        <button disabled={undoStack.current.length <= 1} className={`btn btn-${props.theme} mx-1 my-1`} onClick={handleUndo}>Undo</button>
-        <button disabled={redoStack.current.length <= 1} className={`btn btn-${props.theme} mx-1 my-1`} onClick={handleRedo}>Redo</button>
+        <button disabled={undoStack.current.length === 0} className={`btn btn-${props.theme} mx-1 my-1`} onClick={handleUndo}>Undo</button>
+        <button disabled={redoStack.current.length === 0} className={`btn btn-${props.theme} mx-1 my-1`} onClick={handleRedo}>Redo</button>
     </div>
     <div className='container my-3' style={{color: props.mode=== 'dark'?'white': 'black'}}>
         <h2>Your text Summary</h2>
